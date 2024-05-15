1ed1d4f47a3fcf4a64d3c4f472af908887c612cb
ElJonas <jona16lozada@gmail.com>
2024-05-15 04:53:50 -0500

    Se añadio la clase Laboratorio.java

---
+++ src/Clases/Laboratorio.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Clases;
+
+/**
+ *
+ * @author ASUS GAMER
+ */
+public class Laboratorio extends Aula{
+    private int numComputadoras;
+
+    public Laboratorio(int numComputadoras, Object Bloque, String nombre, String piso, String capacidad) {
+        super(Bloque, nombre, piso, capacidad);
+        this.numComputadoras = numComputadoras;
+    }
+
+    public int getNumComputadoras() {
+        return this.numComputadoras;
+    }
+
+    public void setNumComputadoras(int numComputadoras) {
+        this.numComputadoras = numComputadoras;
+    }
+    
+}
