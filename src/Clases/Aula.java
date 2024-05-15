a31612dfd2336394e06ef7e104ea7369e09cf711
ElJonas <jona16lozada@gmail.com>
2024-05-14 22:53:08 -0500

    se añadio los valores, los getters y setters y un constructor de aula.java

---
+++ src/Clases/Aula.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Clases;
+
+/**
+ *
+ * @author ASUS GAMER
+ */
+public class Aula {
+    private Object Bloque;
+    private String nombre;
+    private String piso;
+    private String capacidad;
+
+    public Aula(Object Bloque, String nombre, String piso, String capacidad) {
+        this.Bloque = Bloque;
+        this.nombre = nombre;
+        this.piso = piso;
+        this.capacidad = capacidad;
+    }
+
+    public Object getBloque() {
+        return this.Bloque;
+    }
+
+    public void setBloque(Object Bloque) {
+        this.Bloque = Bloque;
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPiso() {
+        return this.piso;
+    }
+
+    public void setPiso(String piso) {
+        this.piso = piso;
+    }
+
+    public String getCapacidad() {
+        return this.capacidad;
+    }
+
+    public void setCapacidad(String capacidad) {
+        this.capacidad = capacidad;
+    }
+    
+}
