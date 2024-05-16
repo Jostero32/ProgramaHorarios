8bb461d636860427e133b8c89835b3961a21ac9c
Bryan Pazmino <bryanpazmi1977@gmail.com>
2024-05-15 22:31:11 -0500

    Se agregaron las clases de docente, materia y docenteMateria

---
+++ src/Clases/Docente.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Clases;
+
+/**
+ *
+ * @author Bryan
+ */
+public class Docente {
+    private int id;
+    private String nombre;
+    private String email;
+
+    public Docente(int id, String nombre, String email) {
+        this.id = id;
+        this.nombre = nombre;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
---
+++ src/Clases/DocenteMateria.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Clases;
+
+/**
+ *
+ * @author Bryan
+ */
+public class DocenteMateria {
+    private int docenteId;
+    private int materiaId;
+
+    public DocenteMateria(int docenteId, int materiaId) {
+        this.docenteId = docenteId;
+        this.materiaId = materiaId;
+    }
+
+    // Getters y setters
+    public int getDocenteId() {
+        return docenteId;
+    }
+
+    public void setDocenteId(int docenteId) {
+        this.docenteId = docenteId;
+    }
+
+    public int getMateriaId() {
+        return materiaId;
+    }
+
+    public void setMateriaId(int materiaId) {
+        this.materiaId = materiaId;
+    }
+}
