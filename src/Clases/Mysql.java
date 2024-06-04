52f2f9b1cadb579960be632a162c8997898f98ea
ElJonas <jona16lozada@gmail.com>
2024-06-03 22:32:05 -0500

    Se añadio usuario y contraseña de mi base de datos

--- src/Clases/Mysql.java
+++ src/Clases/Mysql.java
@@ -15,10 +15,12 @@ import java.sql.SQLException;
 public class Mysql {
 
 
-    private static final String URL = "jdbc:mysql://localhost:3306/programaHorarios?characterEncoding=UTF-8";
+   /* private static final String URL = "jdbc:mysql://localhost:3306/programaHorarios?characterEncoding=UTF-8";
     private static final String USER = "root";
-    private static final String PASSWORD = "081005";
-
+    private static final String PASSWORD = "081005";*/
+private static final String URL = "jdbc:mysql://localhost:3306/horarios?characterEncoding=UTF-8";
+    private static final String USER = "root";
+    private static final String PASSWORD = "yPaqryDz24";
     private Connection connection;
 
     public Mysql() {
